8d4f0ca339cd2accbae055d84a30f92350979dc1
ysily8817 <281446639@qq.com>
2019-07-29 23:39:07 +0800

    home 7/29/23:39

---
+++ Chapter11/src/main/java/com/course/Application.java
@@ -0,0 +1,9 @@
+package com.course;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
+@SpringBootApplication
+public class Application {
+}
