66ba8e8688278522997da7c60a23767b6d2c2dca
ysily8817 <281446639@qq.com>
2019-07-30 23:55:53 +0800

    add 11

--- Chapter11/src/main/java/com/course/controller/Demo.java
+++ Chapter11/src/main/java/com/course/controller/Demo.java
@@ -1,4 +1,25 @@
 package com.course.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.log4j.Log4j;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@Log4j
+@RestController
+@Api(value = "/", description = "第一个mybitc Demo")
 public class Demo {
+    //首先获取一个执行sql语句的对象
+    @Autowired
+    private SqlSessionTemplate template;
+
+    @RequestMapping(value = "/getUserCount", method = RequestMethod.GET)
+    @ApiOperation(value = "可以获得用户数",httpMethod = "GET")
+    public int getUserCount() {
+       return template.selectOne("getUserCount");
+    }
 }
